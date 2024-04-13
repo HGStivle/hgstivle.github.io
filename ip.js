6a33e2250f3ba9add8feb62f593b404d00df3b06
Stivle <133083816+HGStivle@users.noreply.github.com>
2024-04-13 10:29:03 +0800

    Add files via upload

--- ip.js
+++ ip.js
@@ -3,7 +3,7 @@ fetch("https://api.vvhan.com/api/ipInfo")
   .then((response) => response.json())
   .then((data) => {
     document.getElementById("ip").textContent = data.ip;
-    document.getElementById("prov").textContent = data.info.country+data.info.prov;
+    document.getElementById("prov").textContent = data.info.country+data.info.prov+data.info.city;
     document.getElementById("city").textContent = data.info.city;
   })
   .catch((error) => console.error(error));
